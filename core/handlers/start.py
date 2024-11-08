f21e858e1ffb49c5207134c47ea8f604fd9e0e35
0sokrat0 <kobelevdaniil3101@yandex.ru>
2024-11-09 00:09:18 +0300

    Шт

---
+++ core/handlers/start.py
@@ -0,0 +1,15 @@
+import logging
+
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram_dialog import StartMode, DialogManager
+from aiogram.types import Message
+
+from core.states.StartSG import StartSG
+
+start_router = Router()
+logger = logging.getLogger(__name__)
+
+@start_router.message(CommandStart())
+async def command_start_process(message: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(state = StartSG.start, mode = StartMode.RESET_STACK)
